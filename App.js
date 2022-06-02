28aedd8f154f017637d284653339c9690312d847
juancamilorivas <bogota.diamond@hotmail.com>
2022-06-02 01:18:28 -0500

    Tabs y SafeAreaView

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Juan Camilo Rivas Molina gano!!</Text>
       <StatusBar style="auto" />
     </View>
   );
